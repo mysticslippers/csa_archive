cb28a4830332d67ce6ebb9608504cf994f8e7e20
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-21 15:55:39 +0300

    update count_zero.asm

--- Lab3_csa/f32a/count_zero.asm
+++ Lab3_csa/f32a/count_zero.asm
@@ -3,30 +3,31 @@ input_addr:    .word 0x80
 output_addr:   .word 0x84
 total:		   .word 0
 
-	.text
+.text
 _start:
 	@p input_addr b!
 	@b
-	
 	lit 31 >r
+
 loop:
 	dup
 	lit 1 and
-    lit -1 +
+    	lit -1 +
 	if continue
-    inc_total	
+    	inc_total
+
 continue:
 	2/
 	next loop
 	write_output ;
 
 inc_total:
-	@p	total
+	@p total
 	lit 1 +
 	!p total
 	;
 	
-	
+
 write_output:
 	@p output_addr b!
 	@p total 
